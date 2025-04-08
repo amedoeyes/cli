76bc16f7d083dc53b0675d0c6a0cb845694935a8
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-04-08 22:22:17 +0200

    build: add warnings

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,24 @@ set_languages("cxx23")
 set_toolchains("clang")
 set_runtimes("c++_shared")
 
+add_cxxflags(
+	"-Weverything",
+	"-Wno-c++98-compat",
+	"-Wno-c++98-compat-pedantic",
+	"-Wno-exit-time-destructors",
+	"-Wno-missing-prototypes",
+	"-Wno-padded",
+	"-Wno-reserved-identifier", --doesn't work correctly with modules
+	"-Wno-shadow-field-in-constructor",
+	"-Wno-switch-default",
+	"-Wno-unsafe-buffer-usage-in-container",
+	"-Wno-weak-vtables"
+)
+
 add_rules("mode.debug", "mode.release", "mode.releasedbg", "mode.check", "mode.profile")
 add_rules("plugin.compile_commands.autoupdate", { lsp = "clangd", outputdir = "build" })
 
 set_policy("build.c++.modules", true)
-
 target("cli", function()
 	set_kind("static")
 	add_files("src/**.cppm", { public = true })
